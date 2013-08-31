a2eb2f795745ca5618351304346affd71cea96e0
Bryan Lewis <blewis@arbitrage.(none)>
2013-08-31 11:54:44 -0400

    Improved service configuration file options

--- init.d/after-install.sh
+++ init.d/after-install.sh
@@ -5,8 +5,18 @@
 # package is installed.
 
 chmod 0755 /etc/init.d/shimsvc
-# Generate a certificate
 mkdir -p /var/lib/shim
+# Write out an example config file to /var/lib/shim/conf
+cat >/var/lib/shim/conf << EOF
+# Shim configuration file
+# Uncomment and change any of the following values. Restart shim for
+# your changes to take effect (default values are shown).
+
+#auth=login
+#ports=8080,8083s
+#scidbport=1239
+EOF
+# Generate a certificate
 openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=US/ST=MA/L=Waltham/O=Paradigm4/CN=$(hostname)" -keyout /var/lib/shim/ssl_cert.pem 2>/dev/null >> /var/lib/shim/ssl_cert.pem
 if test $? -ne 0; then
   echo "SSL certificate generation failed (openssl not found): TLS disabled."
