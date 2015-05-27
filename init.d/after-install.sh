eee9c29bccee2f98e6a76a161ce9522d28379aca
B. W. Lewis <blewis@illposed.net>
2015-05-27 06:27:44 -0400

    Fixed after install script tmp path selection

--- init.d/after-install.sh
+++ init.d/after-install.sh
@@ -13,9 +13,10 @@ TMP=/tmp
 s=`ps aux | grep scidb  | grep "dbname" | head -n 1`
 if test -n "$s"; then
   PORT=`echo "$s" | sed -e "s/.*-p //;s/ .*//"`
-  TMP=`echo "$s" | sed -e "s/.*-s //;s/ .*//" | sed -e "s@\(/[0-9]*/\)[0-9]*/.*@\1@"`
   INS=`echo "$s" | sed -e "s/.*-s //;s/ .*//" | sed -e "s@.*/[0-9]*/\([0-9]*\)/.*@\1@"`
   INS=$(( $INS ))
+  TMP=`echo "$s" | sed -e "s/.*-s //;s/ .*//" | sed -e "s@\(/[0-9]*/\)[0-9]*/.*@\1@"`
+  TMP="${TMP}/${INS}
 fi
 # Write out an example config file to /var/lib/shim/conf
 cat >/var/lib/shim/conf << EOF
