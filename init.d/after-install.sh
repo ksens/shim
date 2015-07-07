1b3f741963fe6e29a5216a2ee13f0c444e1e46ce
bwlewis <blewis@illposed.net>
2015-07-07 20:38:04 +0000

    Fixed bug in after-install.sh script; not sure how that got in there

--- init.d/after-install.sh
+++ init.d/after-install.sh
@@ -16,7 +16,7 @@ if test -n "$s"; then
   INS=`echo "$s" | sed -e "s/.*-s //;s/ .*//" | sed -e "s@.*/[0-9]*/\([0-9]*\)/.*@\1@"`
   INS=$(( $INS ))
   TMP=`echo "$s" | sed -e "s/.*-s //;s/ .*//" | sed -e "s@\(/[0-9]*/\)[0-9]*/.*@\1@"`
-  TMP="${TMP}/${INS}
+  TMP="${TMP}/${INS}"
 fi
 # Write out an example config file to /var/lib/shim/conf
 cat >/var/lib/shim/conf << EOF
@@ -35,10 +35,11 @@ tmp=$TMP
 #timeout=60
 instance=$INS
 EOF
+
 # Generate a certificate
 openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=US/ST=MA/L=Waltham/O=Paradigm4/CN=$(hostname)" -keyout /var/lib/shim/ssl_cert.pem 2>/dev/null >> /var/lib/shim/ssl_cert.pem
 if test $? -ne 0; then
-  echo "SSL certificate generation failed (openssl not found): TLS disabled."
+  echo "SSL certificate generation failed openssl not found: TLS disabled."
   rm -f /var/lib/shim/ssl_cert.pem
 fi
 if test -n "$(which update-rc.d)"; then
