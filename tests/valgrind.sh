502076cca6e6ea296f79f3030649249146b587d1
B. W. Lewis <blewis@illposed.net>
2015-08-07 22:58:58 -0400

    More valgrind tests including TLS and upload

--- tests/valgrind.sh
+++ tests/valgrind.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 host=localhost
 port=8088
+psec=8889
 td=$(mktemp -d)
 
 function fail {
   echo "FAIL"
   rm -rf $td
-  kill -9 %1
+  kill -15 %1
+  sleep 1
+  kill -9 %1 2>/dev/null
   exit 1
 }
 
 mkdir -p $td/wwwroot
 echo "homer:elmo" > $td/wwwroot/.htpasswd
 openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=US/ST=MA/L=Waltham/O=Paradigm4/CN=$(hostname)" -keyout $td/ssl_cert.pem 2>/dev/null >> $td/ssl_cert.pem
-valgrind --log-file=/tmp/valgrind.out --leak-check=yes ./shim -p $port -r $td/wwwroot  -f  &
+valgrind --log-file=/tmp/valgrind.out --leak-check=yes ./shim -p "${port},${psec}s" -r $td/wwwroot  -f  &
 sleep 5
 
 id=$(curl -f -s --digest --user homer:elmo "http://${host}:${port}/new_session" | sed -e "s/.*//")
 curl -f -s --digest --user homer:elmo "http://${host}:${port}/execute_query?id=${id}&query=list('functions')&save=dcsv" || fail
 x=1
 while test $x -lt 40;do
-  curl  -s --digest --user homer:elmo "http://${host}:${port}/read_bytes?id=${id}&n=555" 2>/dev/null  || break
+  curl  -s --digest --user homer:elmo "http://${host}:${port}/read_bytes?id=${id}&n=555" 2>/dev/null | wc -l  || break
   x=$(($x + 1))
 done
 curl -f -s  --digest --user homer:elmo "http://${host}:${port}/release_session?id=${id}" >/dev/null  || fail
@@ -28,7 +31,7 @@ curl -f -s  --digest --user homer:elmo "http://${host}:${port}/release_session?i
 
 # Basic compressed stream example using default compression
 # The ugly url-encoded query string below is build(<x:double>[i=1:1000000,10000,0],i)
-echo "Compressed stream download...this can take a few minutes..."
+echo "Compressed stream download, this can take as long as a few minutes..."
 id=$(curl -f -s --digest --user homer:elmo "http://${host}:${port}/new_session" | sed -e "s/.*//")
 echo $id
 curl -f -s --digest --user homer:elmo "http://${host}:${port}/execute_query?id=${id}&query=build(%3Cx%3Adouble%3E%5Bi%3D1%3A1000000%2C10000%2C0%5D%2Ci)&save=(double)&stream=1&compression=3"
@@ -37,10 +40,10 @@ curl -f -s --digest --user homer:elmo "http://${host}:${port}/read_bytes?id=${id
 
 # Uncompressed stream of binary data using TLS
 echo "Uncompressed data download over TLS"
-id=$(curl -k -f -s --digest --user homer:elmo "https://${host}:${port}/new_session" | sed -e "s/.*//")
+id=$(curl -f -s -k --digest --user homer:elmo "https://${host}:${psec}/new_session" | sed -e "s/.*//")
 echo $id
-curl -k -f -s --digest --user homer:elmo "https://${host}:${port}/execute_query?id=${id}&query=build(%3Cx%3Adouble%3E%5Bi%3D1%3A1000000%2C10000%2C0%5D%2Ci)&save=(double)&stream=1"
-curl -k -f -s --digest --user homer:elmo "https://${host}:${port}/read_bytes?id=${id}&n=0" | dd of=/dev/null || fail
+curl -f -s -k --digest --user homer:elmo "https://${host}:${psec}/execute_query?id=${id}&query=build(%3Cx%3Adouble%3E%5Bi%3D1%3A1000000%2C10000%2C0%5D%2Ci)&save=(double)&stream=1"
+curl -f -s -k --digest --user homer:elmo "https://${host}:${psec}/read_bytes?id=${id}&n=0" | dd of=/dev/null || fail
 
 # Data upload
 echo "Uncompressed data upload (may take a while...)"
